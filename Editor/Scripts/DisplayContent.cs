94479509ee01ee94eefbf78bd5ac33c149cda243
Tiogiras <tiogiras@gmail.com>
2023-10-24 21:21:32 +0200

    Tab Logic

---
+++ Editor/Scripts/DisplayContent.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using UnityEngine.UIElements;
+
+namespace Editor.Scripts
+{
+
+public static partial class DisplayContent
+{
+    
+    #region Private Methods
+
+    private static partial void Validators(VisualElement root)
+    {
+        var tabs = root.Q <GroupBox>("Tabs");
+        var tabContentParent = root.Q <GroupBox>("TabContent");
+        
+        RegisterTabCallbacks(tabs, tabContentParent, 3);
+        
+        SetTabContentLocations(
+            "User Interface/Display Content Tabs/Tab0",
+            "User Interface/Display Content Tabs/Tab1",
+            "User Interface/Display Content Tabs/Tab2");
+    }
+
+    #endregion
+}
+
+}
+#endif
