cd1de637cd0d80e869ad24c5c00435760dd402cf
Tiogiras <tiogiras@gmail.com>
2024-06-05 15:25:22 +0200

    Added basic unit tests

---
+++ Editor/Scripts/Tests/PackageTests.cs
@@ -0,0 +1,40 @@
+﻿#if UNITY_EDITOR
+#if UNITY_INCLUDE_TESTS
+using System.Collections;
+using System.Threading.Tasks;
+using MegaPint.Editor.Scripts.PackageManager.Packages;
+using MegaPint.Editor.Scripts.Tests.Utility;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace MegaPint.Editor.Scripts.Tests
+{
+
+/// <summary> Unit tests regarding the general structure and settings of the package </summary>
+internal class PackageTests
+{
+    private static bool s_initialized;
+    
+    [UnityTest] [Order(0)]
+    public IEnumerator InitializePackageCache()
+    {
+        Task <bool> task = TestsUtility.CheckCacheInitialization();
+        yield return task.AsIEnumeratorReturnNull();
+
+        s_initialized = task.Result;
+        Assert.IsTrue(task.Result);
+    }
+
+    [Test] [Order(1)]
+    public void PackageStructure()
+    {
+        if (!s_initialized)
+            Assert.Fail("FAILED ===> Missing packageCache initialization!");
+        
+        TestsUtility.CheckStructure(PackageKey.AlphaButton);
+    }
+    
+}
+}
+#endif
+#endif
