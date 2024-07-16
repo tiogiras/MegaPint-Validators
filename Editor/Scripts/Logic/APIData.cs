ae991b2f522f3d23dc07982d32e578d0020e2f43
Tiogiras <tiogiras@gmail.com>
2024-07-16 15:48:12 +0200

    CleanUp

---
+++ Editor/Scripts/Logic/APIData.cs
@@ -0,0 +1,308 @@
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+
+namespace MegaPint.Editor.Scripts.Logic
+{
+
+/// <summary> Holds information about the apis listed in the api tab </summary>
+internal static class APIData
+{
+    /// <summary> Data container for any api </summary>
+    public class Data
+    {
+        public string displayName;
+        public int indentLevel;
+        public List <Data> subAPIs;
+        public string title;
+    }
+
+    private static readonly List <Data> s_data = new()
+    {
+        new Data
+        {
+            title = "ValidationRequirementNameAttribute",
+            displayName = "ValidationRequirementNameAttribute",
+            indentLevel = 0,
+            subAPIs = new List <Data>
+            {
+                new()
+                {
+                    title = "bool : ValidationRequirementNameAttribute.allowMultiple",
+                    displayName = "allowMultiple",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title =
+                        "List<Type> : ValidationRequirementNameAttribute.incompatibleRequirements",
+                    displayName = "incompatibleRequirements",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "int[] : ValidationRequirementNameAttribute.menuOrder",
+                    displayName = "menuOrder",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "string : ValidationRequirementNameAttribute.name",
+                    displayName = "name",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "Type : ValidationRequirementNameAttribute.requirementType",
+                    displayName = "requirementType",
+                    indentLevel = 1
+                }
+            }
+        },
+        new Data
+        {
+            title = "ScriptableValidationRequirement",
+            displayName = "ScriptableValidationRequirement",
+            indentLevel = 0,
+            subAPIs = new List <Data>
+            {
+                new()
+                {
+                    title = "void : ScriptableValidationRequirement.AddError()",
+                    displayName = "AddError()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "void : ScriptableValidationRequirement.AddErrorIf()",
+                    displayName = "AddErrorIf()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "void : ScriptableValidationRequirement.AddErrors()",
+                    displayName = "AddErrors()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "void : ScriptableValidationRequirement.OnRequirementValidation()",
+                    displayName = "OnRequirementValidation()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "void : ScriptableValidationRequirement.Validate()",
+                    displayName = "Validate()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "bool : ScriptableValidationRequirement.initialized",
+                    displayName = "initialized",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "Object : ScriptableValidationRequirement.objectReference",
+                    displayName = "objectReference",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "Object : ScriptableValidationRequirement.preventListHeaderIssue",
+                    displayName = "preventListHeaderIssue",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "Object : ScriptableValidationRequirement.severityOverwrite",
+                    displayName = "severityOverwrite",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "Object : ScriptableValidationRequirement.targetGameObject",
+                    displayName = "targetGameObject",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "Object : ScriptableValidationRequirement.uniqueID",
+                    displayName = "uniqueID",
+                    indentLevel = 1
+                }
+            }
+        },
+        new Data
+        {
+            title = "ValidatableMonoBehaviour",
+            displayName = "ValidatableMonoBehaviour",
+            indentLevel = 0,
+            subAPIs = new List <Data>
+            {
+                new()
+                {
+                    title = "struct : ValidatableMonoBehaviour.SettingPriority",
+                    displayName = "SettingPriority",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "void : ValidatableMonoBehaviour.BeforeValidation()",
+                    displayName = "BeforeValidation()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "string[] : ValidatableMonoBehaviour.DefaultImports()",
+                    displayName = "DefaultImports()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title =
+                        "List<ValidatorSettings> : ValidatableMonoBehaviour.GetImportedSettings()",
+                    displayName = "GetImportedSettings()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "void : ValidatableMonoBehaviour.ImportSetting(ValidatorSettings)",
+                    displayName = "ImportSetting()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title =
+                        "void : ValidatableMonoBehaviour.RemoveImportedSetting(ValidatorSettings)",
+                    displayName = "RemoveImportedSetting()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title =
+                        "List <ScriptableValidationRequirement> : ValidatableMonoBehaviour.Requirements(bool = false)",
+                    displayName = "Requirements()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title =
+                        "void : ValidatableMonoBehaviour.SetRequirements(List <ScriptableValidationRequirement>)",
+                    displayName = "SetRequirements()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title =
+                        "ValidationState : ValidatableMonoBehaviour.Validate(out List <ValidationError>)",
+                    displayName = "Validate()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "bool : ValidatableMonoBehaviour.HasImportedSettings",
+                    displayName = "HasImportedSettings",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title =
+                        "List <ValidatorSettings> : ValidatableMonoBehaviour.defaultSettings",
+                    displayName = "defaultSettings",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "bool : ValidatableMonoBehaviour.importedSettingsFoldout",
+                    displayName = "importedSettingsFoldout",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title =
+                        "List <SettingPriority> : ValidatableMonoBehaviour.settingPriorities",
+                    displayName = "settingPriorities",
+                    indentLevel = 1
+                }
+            }
+        },
+        new Data
+        {
+            title = "ValidationError",
+            displayName = "ValidationError",
+            indentLevel = 0,
+            subAPIs = new List <Data>
+            {
+                new() {title = "ValidationError.errorName", displayName = "errorName", indentLevel = 1},
+                new() {title = "ValidationError.errorText", displayName = "errorText", indentLevel = 1},
+                new() {title = "ValidationError.fixAction", displayName = "fixAction", indentLevel = 1},
+                new() {title = "ValidationError.gameObject", displayName = "gameObject", indentLevel = 1},
+                new() {title = "ValidationError.severity", displayName = "severity", indentLevel = 1}
+            }
+        },
+        new Data
+        {
+            title = "ValidationRequirementMetaData",
+            displayName = "ValidationRequirementMetaData",
+            indentLevel = 0,
+            subAPIs = new List <Data>
+            {
+                new()
+                {
+                    title = "void : ValidationRequirementMetaData.OnInitialization()",
+                    displayName = "OnInitialization()",
+                    indentLevel = 1
+                }
+            }
+        },
+        new Data
+        {
+            title = "ValidatorSettings",
+            displayName = "ValidatorSettings",
+            indentLevel = 0,
+            subAPIs = new List <Data>
+            {
+                new()
+                {
+                    title =
+                        "List <ScriptableValidationRequirement> : Requirements(bool = false)",
+                    displayName = "Requirements()",
+                    indentLevel = 1
+                },
+                new()
+                {
+                    title = "void : SetRequirements(List <ScriptableValidationRequirement>)",
+                    displayName = "SetRequirements()",
+                    indentLevel = 1
+                }
+            }
+        },
+        new Data
+        {
+            title = "SerializeReferenceDropdownAttribute",
+            displayName = "SerializeReferenceDropdownAttribute",
+            indentLevel = 0
+        },
+        new Data
+        {
+            title = "ValidationRequirementTooltipAttribute",
+            displayName = "ValidationRequirementTooltipAttribute",
+            indentLevel = 0
+        },
+        new Data {title = "ToggleableSetting", displayName = "ToggleableSetting", indentLevel = 0},
+        new Data {title = "ValidationState", displayName = "ValidationState", indentLevel = 0}
+    };
+
+    #region Public Methods
+
+    public static List <Data> Get()
+    {
+        return s_data;
+    }
+
+    #endregion
+}
+
+}
+#endif
---
+++ Editor/Scripts/Logic/APILogic.cs
@@ -0,0 +1,252 @@
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+using System.Linq;
+using MegaPint.Editor.Scripts.GUI;
+using UnityEngine;
+using UnityEngine.UIElements;
+using GUIUtility = MegaPint.Editor.Scripts.GUI.Utility.GUIUtility;
+
+namespace MegaPint.Editor.Scripts.Logic
+{
+
+/// <summary> Logic for the api tab of the validators package </summary>
+internal static class APILogic
+{
+    private static readonly List <APIData.Data> s_openAPIs = new();
+    private static List <APIData.Data> s_topLevelAPI = new();
+    private static List <APIData.Data> s_displayedAPIs = new();
+
+    private static APIData.Data s_selectedAPI;
+
+    private static VisualTreeAsset s_listItemTemplate;
+
+    private static ListView s_listView;
+
+    private static VisualElement s_rightPane;
+    private static VisualElement s_content;
+    private static Label s_title;
+
+    #region Public Methods
+
+    /// <summary> Logic for displaying the api tab of the validators package </summary>
+    /// <param name="root"> RootVisualElement </param>
+    public static void ValidatorsAPILogic(VisualElement root)
+    {
+        var leftPane = root.Q <VisualElement>("LeftPane");
+        s_rightPane = root.Q <VisualElement>("RightPane");
+
+        s_rightPane.style.display = DisplayStyle.None;
+
+        s_listView = leftPane.Q <ListView>("Entries");
+        s_title = s_rightPane.Q <Label>("Title");
+        s_content = s_rightPane.Q <VisualElement>("Content");
+
+        s_listItemTemplate ??= Resources.Load <VisualTreeAsset>(Constants.Validators.UserInterface.APIItem);
+
+        s_listView.makeItem = () => GUIUtility.Instantiate(s_listItemTemplate);
+
+        s_listView.bindItem = BindItem;
+
+        s_listView.selectedIndicesChanged += OnSelection;
+
+        s_displayedAPIs = APIData.Get();
+
+        s_topLevelAPI = s_displayedAPIs.Where(api => api.indentLevel == 0).ToList();
+
+        s_listView.selectedIndex = -1;
+        s_listView.itemsSource = s_displayedAPIs;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary> Add all subAPIs of the target data to the displayed apis </summary>
+    /// <param name="data"> Target data </param>
+    private static void AddSubAPIs(APIData.Data data)
+    {
+        if (data.subAPIs is not {Count: > 0})
+            return;
+
+        var index = s_displayedAPIs.IndexOf(data) + 1;
+
+        s_displayedAPIs.InsertRange(index, data.subAPIs);
+    }
+
+    /// <summary> Bind an list element </summary>
+    /// <param name="element"> Target element </param>
+    /// <param name="index"> Index of the target element in the listView </param>
+    private static void BindItem(VisualElement element, int index)
+    {
+        var api = (APIData.Data)s_listView.itemsSource[index];
+
+        var hierarchy = element.Q <VisualElement>("Hierarchy");
+        hierarchy.Clear();
+
+        if (!s_topLevelAPI.Contains(api))
+        {
+            APIData.Data nextAPI = index == s_listView.itemsSource.Count - 1
+                ? null
+                : (APIData.Data)s_listView.itemsSource[index + 1];
+
+            var nextApiIsSameClass = api.indentLevel == nextAPI?.indentLevel;
+
+            for (var j = 0; j < api.indentLevel; j++)
+            {
+                if (j == api.indentLevel - 1)
+                    hierarchy.Add(nextApiIsSameClass ? DrawFull() : DrawCorner());
+                else
+                    hierarchy.Add(DrawLine());
+            }
+        }
+
+        var label = element.Q <Label>("Name");
+        label.text = api.displayName;
+
+        var open = element.Q <VisualElement>("Open");
+        var closed = element.Q <VisualElement>("Closed");
+
+        var hasSubAPIs = api.subAPIs?.Count > 0;
+        var opened = s_openAPIs.Contains(api);
+
+        open.style.display = opened && hasSubAPIs ? DisplayStyle.Flex : DisplayStyle.None;
+        closed.style.display = !opened && hasSubAPIs ? DisplayStyle.Flex : DisplayStyle.None;
+
+        var isSelectedAPI = s_selectedAPI == api;
+
+        label.style.borderRightWidth = isSelectedAPI ? 3 : 0;
+    }
+
+    /// <summary> Display the right pane </summary>
+    /// <param name="data"> Target data </param>
+    /// <param name="rightPane"> Right pane </param>
+    /// <param name="title"> Title inside the right pane </param>
+    /// <param name="content"> Content inside the right pane </param>
+    private static void DisplayRightPane(APIData.Data data, VisualElement rightPane, Label title, VisualElement content)
+    {
+        rightPane.style.display = DisplayStyle.Flex;
+
+        title.text = data.title;
+    }
+
+    /// <summary> Draw a hierarchy corner </summary>
+    /// <returns> Corner as visualElement </returns>
+    private static VisualElement DrawCorner()
+    {
+        VisualElement ve = DrawEmpty();
+
+        var left = new VisualElement {style = {flexGrow = 1}};
+        ve.Add(left);
+
+        var right = new VisualElement {style = {flexGrow = 1, flexDirection = FlexDirection.Column}};
+        ve.Add(right);
+
+        var rightUp = new VisualElement {style = {flexGrow = 1, borderBottomWidth = 2, borderLeftWidth = 2}};
+        rightUp.AddToClassList(StyleSheetClasses.Border.Color.Separator);
+        right.Add(rightUp);
+
+        var rightDown = new VisualElement {style = {flexGrow = 1}};
+        right.Add(rightDown);
+
+        return ve;
+    }
+
+    /// <summary> Draw a hierarchy empty </summary>
+    /// <returns> Empty as visualElement </returns>
+    private static VisualElement DrawEmpty()
+    {
+        return new VisualElement {style = {width = 20, flexGrow = 0, flexDirection = FlexDirection.Row}};
+    }
+
+    /// <summary> Draw a hierarchy full </summary>
+    /// <returns> Full as visualElement </returns>
+    private static VisualElement DrawFull()
+    {
+        VisualElement ve = DrawEmpty();
+
+        var left = new VisualElement {style = {flexGrow = 1, borderRightWidth = 2}};
+        left.AddToClassList(StyleSheetClasses.Border.Color.Separator);
+        ve.Add(left);
+
+        var right = new VisualElement {style = {flexGrow = 1, flexDirection = FlexDirection.Column}};
+        ve.Add(right);
+
+        var rightUp = new VisualElement {style = {flexGrow = 1, borderBottomWidth = 2}};
+        rightUp.AddToClassList(StyleSheetClasses.Border.Color.Separator);
+        right.Add(rightUp);
+
+        var rightDown = new VisualElement {style = {flexGrow = 1}};
+        right.Add(rightDown);
+
+        return ve;
+    }
+
+    /// <summary> Draw a hierarchy line </summary>
+    /// <returns> Line as visualElement </returns>
+    private static VisualElement DrawLine()
+    {
+        VisualElement ve = DrawEmpty();
+
+        var left = new VisualElement {style = {flexGrow = 1, borderRightWidth = 2}};
+        left.AddToClassList(StyleSheetClasses.Border.Color.Separator);
+        ve.Add(left);
+
+        var right = new VisualElement {style = {flexGrow = 1, flexDirection = FlexDirection.Column}};
+        ve.Add(right);
+
+        return ve;
+    }
+
+    /// <summary> List View selection event </summary>
+    /// <param name="_"> Callback event </param>
+    private static void OnSelection(IEnumerable <int> _)
+    {
+        var index = s_listView.selectedIndex;
+
+        if (index < 0)
+            return;
+
+        var api = (APIData.Data)s_listView.itemsSource[index];
+
+        if (s_selectedAPI == api)
+        {
+            if (s_openAPIs.Contains(api))
+                RemoveSubAPIs(api);
+            else
+            {
+                s_openAPIs.Add(api);
+                AddSubAPIs(api);
+            }
+        }
+
+        s_selectedAPI = api;
+        DisplayRightPane(api, s_rightPane, s_title, s_content);
+        s_listView.RefreshItems();
+
+        s_listView.selectedIndex = -1;
+    }
+
+    /// <summary> Remove all subAPIs of the target data from the displayed apis </summary>
+    /// <param name="data"> Target data </param>
+    private static void RemoveSubAPIs(APIData.Data data)
+    {
+        if (data.subAPIs is not {Count: > 0})
+            return;
+
+        if (!s_openAPIs.Contains(data))
+            return;
+
+        s_openAPIs.Remove(data);
+
+        foreach (APIData.Data subAPI in data.subAPIs)
+        {
+            s_displayedAPIs.Remove(subAPI);
+            RemoveSubAPIs(subAPI);
+        }
+    }
+
+    #endregion
+}
+
+}
+#endif
